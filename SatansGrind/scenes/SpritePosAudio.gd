613ccc2473ffbef609c076968e29c7990d554cff
arthur109 <arthurfaj@gmail.com>
2019-10-05 14:11:00 -0700

    Pure Goodness
    fixed up, devivided into scenes

--- SatansGrind/scenes/SpritePosAudio.gd
+++ SatansGrind/scenes/SpritePosAudio.gd
@@ -9,8 +9,3 @@ func _ready():
 	get_viewport().audio_listener_enable_2d = true
 	$AudioStreamPlayer2D.play()
 
-func _porcess(delta):
-	if self.position.x > 0 - self.texture.get_width():
-		move_local_x(-1)
-	else:
-		self.position.x = get_viewport().get_visible_rect().size.x
---
+++ SatansGrind/scripts/AudioStreamPlayer2D.gd
@@ -0,0 +1,14 @@
+extends AudioStreamPlayer2D
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_viewport().audio_listener_enable_2d = true
+	play()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
